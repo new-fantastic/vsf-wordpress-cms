98b826d1b867e77872c3d15d87c89c16386e45cb
Aleksander Grygier <alek@grygier.co>
2019-05-05 21:25:50 +0200

    modified isExternal utility

--- util/IsLinkExternal.ts
+++ util/IsLinkExternal.ts
@@ -2,5 +2,5 @@ export default (link: string): Boolean => {
   // External rules
   // Starts with http/https
   // Starts with //*.\.*.
-  return link.indexOf('http') === 0 || /\/\/*.\.*./.test(link)
+  return link.indexOf('http') === 0 //|| /\/\/*.\.*./.test(link)
 }
