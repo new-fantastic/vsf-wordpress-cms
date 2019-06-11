eeecd95f2f1f22c972d2a2500d3df6e825327bb5
Fifciuu <filip.jdrasik@gmail.com>
2019-06-11 11:26:44 +0200

    loadSections, page, post works

--- hooks/afterRegistration.ts
+++ hooks/afterRegistration.ts
@@ -1,5 +1,5 @@
 import { AsyncDataLoader } from '@vue-storefront/core/lib/async-data-loader'
-
+import config from 'config'
 import WpJson from '@vue-wordpress/core'
 import * as vuex from '@vue-wordpress/core/plugin/initializers/store'
 // import registerPlugin from '@vue-wordpress/core/plugin/registerPlugin'
@@ -8,44 +8,26 @@ export function afterRegistration ({ Vue, store, isServer }) {
 
   const tmpCfg: any = {
     config: {
-      url: 'https://test.wp.newfantastic.com/',
-      lang: 'pl'
+      ...config.wordpressCms
     },
-    store,
-    router: false
+    store: true,
+    router: true
   }
+
+  Vue.use(WpJson, tmpCfg)
 
   AsyncDataLoader.push({
     execute: async ({ route }) => {
-      // const lang = getLangByRoute(route)
-
-      // vuex.registerModules(store)
-      Vue.use(WpJson, tmpCfg)
 
       // HERE WILL BE PRODUCTS
 
-      // await vuex.loadBase(store.dispatch, 'menus' in tmpCfg.config ? tmpCfg.config.menus : {}) // menus
-      // vuex.setLang(store.commit, tmpCfg.config.lang) 
-      // vuex.setConfig(store.commit, {
-      //   ...tmpCfg.config,
-      //   asyncData: true
-      // }) 
+      await vuex.loadBase(store.dispatch, 'menus' in tmpCfg.config ? tmpCfg.config.menus : {}) // menus
+      vuex.setLang(store.commit, tmpCfg.config.lang) 
+      vuex.setConfig(store.commit, {
+        ...tmpCfg.config,
+        asyncData: true
+      }) 
 
-      // store.commit(`wp_rest_content/${SET_LANG}`, lang)
-      
-      // They'll be loaded in parallel
-      // await store.dispatch('wp_rest_content/loadMenu', {
-      //   menuSlugs: config.wordpressCms.menus,
-      //   lang
-      // })
-
-      // await store.dispatch('wp_rest_content/loadMeta', {
-      //   lang
-      // })
-
-      // await store.dispatch('wpr_media/loadMedia', {
-      //   lang
-      // })
     }
   })
 }
--- index.ts
+++ index.ts
@@ -5,7 +5,14 @@ import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/
 import { preparedRoutes } from './router/routes'
 import { ModulePrefix } from '@vue-wordpress/core'
 
-// import { config } from '@vue-wordpress/core/'
+import { config } from '@vue-wordpress/core/store/config'
+import { lang } from '@vue-wordpress/core/store/lang'
+import { layouts } from '@vue-wordpress/core/store/layouts'
+import { media } from '@vue-wordpress/core/store/media'
+import { menu } from '@vue-wordpress/core/store/menu'
+import { meta } from '@vue-wordpress/core/store/meta'
+import { page } from '@vue-wordpress/core/store/page'
+import { post } from '@vue-wordpress/core/store/post'
 
 export const KEY = 'wp_rest_content'
 // export const MEDIA_KEY = 'wpr_media'
@@ -15,10 +22,18 @@ const moduleConfig: VueStorefrontModuleConfig = {
   afterRegistration,
   router: { 
     routes: <any>preparedRoutes
-  }//,
-  // store: { modules: [
-  //   { key: `${ModulePrefix}_config`, module:  }
-  // ] },
+  },
+  store: { modules: [
+    { key: `${ModulePrefix}_config`, module: config },
+    { key: `${ModulePrefix}_lang`, module: lang },
+    { key: `${ModulePrefix}_layouts`, module: layouts },
+    { key: `${ModulePrefix}_media`, module: media },
+    { key: `${ModulePrefix}_menu`, module: menu },
+    { key: `${ModulePrefix}_meta`, module: meta },
+    { key: `${ModulePrefix}_page`, module: page },
+    { key: `${ModulePrefix}_post`, module: post },
+
+  ] },
 }
 
 export const WpJson = new VueStorefrontModule(moduleConfig)
