96ca1e1ae55468b6eb893bfe735374d9fe3bd5eb
Fifciuu <filip.jdrasik@gmail.com>
2019-08-30 14:40:24 +0200

    Upd

--- hooks/afterRegistration.ts
+++ hooks/afterRegistration.ts
@@ -1,8 +1,9 @@
-import { AsyncDataLoader } from '@vue-storefront/core/lib/async-data-loader'
-import config from 'config'
-import WpJson from '@vue-wordpress/core'
-import * as vuex from '@vue-wordpress/core/plugin/initializers/store'
-// import registerPlugin from '@vue-wordpress/core/plugin/registerPlugin'
+import { AsyncDataLoader } from "@vue-storefront/core/lib/async-data-loader";
+import config from "config";
+import WpJson from "@vue-wordpress/core";
+import * as vuex from "@vue-wordpress/core/plugin/initializers/store";
+import * as types from "@vue-wordpress/core/store/config/mutation-types";
+import { currentStoreView } from "@vue-storefront/core/lib/multistore";
 
 export function afterRegistration({ Vue, store, isServer }) {
 
@@ -10,16 +11,25 @@ export function afterRegistration({ Vue, store, isServer }) {
     ...config.wordpressCms,
     store: true,
     router: true
-  }
+  };
 
-  if ('plugins' in config.wordpressCms) {
-    tmpCfg.plugins = config.wordpressCms.plugins
+  if ("plugins" in config.wordpressCms) {
+    tmpCfg.plugins = config.wordpressCms.plugins;
   }
-
-  Vue.use(WpJson, tmpCfg)
+  Vue.use(WpJson, tmpCfg);
 
   AsyncDataLoader.push({
     execute: async ({ route }) => {
+      const { storeCode } = currentStoreView();
+      if (storeCode && storeCode.length > 0 && storeCode !== "pl") {
+        // vuex.setConfig(store.commit, {
+        //   ...tmpCfg,
+        //   requestPrefix: storeCode,
+        //   asyncData: true
+        // });
+        Vue.prototype.$wp.requestPrefix = storeCode;
+      } else {
+      }
 
       // HERE WILL BE PRODUCTS
 
@@ -27,8 +37,7 @@ export function afterRegistration({ Vue, store, isServer }) {
       vuex.setConfig(store.commit, {
         ...tmpCfg,
         asyncData: true
-      })
-
+      });
     }
-  })
+  });
 }
--- index.ts
+++ index.ts
@@ -1,45 +1,47 @@
 // import { module } from './store'
 // import { mediaModule } from './store/media'
-import { afterRegistration } from './hooks/afterRegistration'
-import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
-import { preparedRoutes } from './router/routes'
-import { ModulePrefix } from '@vue-wordpress/core'
+import { afterRegistration } from "./hooks/afterRegistration";
+import {
+  VueStorefrontModule,
+  VueStorefrontModuleConfig
+} from "@vue-storefront/core/lib/module";
+import { preparedRoutes } from "./router/routes";
+import { ModulePrefix } from "@vue-wordpress/core";
 
-import { config } from '@vue-wordpress/core/store/config'
-import { layouts } from '@vue-wordpress/core/store/layouts'
-import { media } from '@vue-wordpress/core/store/media'
-import { menu } from '@vue-wordpress/core/store/menu'
-import { meta } from '@vue-wordpress/core/store/meta'
-import { post } from '@vue-wordpress/core/store/post'
-import cfg from 'config'
+import { config } from "@vue-wordpress/core/store/config";
+import { layouts } from "@vue-wordpress/core/store/layouts";
+import { menu } from "@vue-wordpress/core/store/menu";
+import { meta } from "@vue-wordpress/core/store/meta";
+import { post } from "@vue-wordpress/core/store/post";
+import cfg from "config";
 
-export const KEY = 'wp_rest_content'
+export const KEY = "wp_rest_content";
 // export const MEDIA_KEY = 'wpr_media'
 
 let router: any = {
   routes: <any>preparedRoutes
-}
-if ('router' in cfg.wordpressCms) {
+};
+if ("router" in cfg.wordpressCms) {
   if (cfg.wordpressCms.router === false) {
-    router = {}
+    router = {};
   }
 }
 
 const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
   afterRegistration,
-  router: { 
+  router: {
     ...router
   },
-  store: { modules: [
-    { key: `${ModulePrefix}_config`, module: config },
-    { key: `${ModulePrefix}_layouts`, module: layouts },
-    { key: `${ModulePrefix}_media`, module: media },
-    { key: `${ModulePrefix}_menu`, module: menu },
-    { key: `${ModulePrefix}_meta`, module: meta },
-    { key: `${ModulePrefix}_post`, module: post },
+  store: {
+    modules: [
+      { key: `${ModulePrefix}_config`, module: config },
+      { key: `${ModulePrefix}_layouts`, module: layouts },
+      { key: `${ModulePrefix}_menu`, module: menu },
+      { key: `${ModulePrefix}_meta`, module: meta },
+      { key: `${ModulePrefix}_post`, module: post }
+    ]
+  }
+};
 
-  ] },
-}
-
-export const WpJson = new VueStorefrontModule(moduleConfig)
+export const WpJson = new VueStorefrontModule(moduleConfig);
