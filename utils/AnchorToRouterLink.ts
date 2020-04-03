9084791f0f41ea2ad3c02e41e553c4a2569210cc
Fifciuu <filip@newfantastic.com>
2020-04-03 18:14:34 +0200

    RouteUp support

--- utils/AnchorToRouterLink.ts
+++ utils/AnchorToRouterLink.ts
@@ -1,13 +1,30 @@
 import { currentStoreView } from '@vue-storefront/core/lib/multistore';
 // Changes a hrefs to router links with localized route
 // REQUIRES VUE FULL BUILD
-export default (anchor: string): string => {
+export default function(anchor: string): string {
   const { storeCode } = currentStoreView()
 
   return anchor.replace(/href=(["'])/g, 'to=$1')
-  .replace(/(to)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/, (match, to, value) => {
+  .replace(/(to)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/, (match, to, val) => {
     let target
-    if (value.startsWith(`/${storeCode}/`)) {
+    const value = val.trim()
+    const routeUpOptions = [
+      'http://routeUp',
+      'https://routeUp',
+      'routeUp',
+      'http://routeUp/',
+      'https://routeUp/',
+      'routeUp/',
+    ]
+    if (routeUpOptions.includes(value)) {
+      const parts = this.$route.path.split('/')
+      if (parts.length > 1) {
+        // cut store code
+        parts.pop()
+      }
+      const result = parts.join('/')
+      target = !result.startsWith('/') ? `/${result}` : result
+    } else if (value.startsWith(`/${storeCode}/`)) {
       target = value 
     } else if (value.startsWith(`${storeCode}/`)) {
       target = '/' + value 
